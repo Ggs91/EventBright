0159de61b9d2f731a55389a22b65390bbd6e5443
Ggs91 <georges_atalla@hotmail.fr>
2020-12-25 10:35:30 +0100

    add event associtiations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+  # Associations
   belongs_to :administrator
+  has_many :participations, dependent: :destroy
+  has_many :participants, through: :participations, source: :user, dependent: :destroy
+
 end
