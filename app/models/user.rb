9a9ba686f7f2ac5a5c4d88788ed0db6abc761aad
Ggs91 <georges_atalla@hotmail.fr>
2020-12-25 10:26:06 +0100

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :attended_events, through: :participations, source: :event, dependent: :destroy
   has_many :administrated_events, foreign_key: "administrator_id", class_name: "Event", dependent: :destroy
+  # Validations
+  validates :first_name, :last_name,
+     presence: true,
+     on: :update
+  validates :description,
+     presence: true,
+     length: { minimum: 5 },
+     on: :update
 end
