6d65aea6a91d18f0df0d78fab131429c0b4b6c89
Ggs91 <georges_atalla@hotmail.fr>
2020-12-30 15:40:55 +0100

    restrict owner access to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :restrict_acess_to_owner, only: [:show]
+
   def show
+  end
+
+private
+  def restrict_acess_to_owner
+    flash[:warning] = "Sorry, but you are only allowed to view your own profile page." unless authenticate_user! && User.find(params[:id]) == current_user 
+    redirect_to root_path
   end
 end
