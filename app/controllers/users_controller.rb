c3ca55d924ef0d5df1d12fcda1a415409cd9d91c
Ggs91 <georges_atalla@hotmail.fr>
2020-12-30 21:57:59 +0100

    add edit & update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   before_action :restrict_acess_to_owner, only: [:show]
+  before_action :set_user, only: [:edit, :update]
 
   def show
+  end
+
+  def edit
+    @user
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your infos have been edited successfully !"
+      redirect_to @user
+    else
+      flash.now[:warning] = "Could not edit your infos"
+      render :edit
+    end
   end
 
 private
@@ -10,5 +25,13 @@ private
       flash[:warning] = "Sorry, but you are only allowed to view your own profile page." 
       redirect_to root_path
     end
+  end
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :description)
   end
 end
