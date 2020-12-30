5004ba69474e35e7ac97dbbbf1f656658fda8d2d
Ggs91 <georges_atalla@hotmail.fr>
2020-12-30 16:09:52 +0100

    refactor #restrict_user_to_owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
 
 private
   def restrict_acess_to_owner
-    flash[:warning] = "Sorry, but you are only allowed to view your own profile page." unless authenticate_user! && User.find(params[:id]) == current_user 
-    redirect_to root_path
+    unless User.find(params[:id]) == current_user 
+      flash[:warning] = "Sorry, but you are only allowed to view your own profile page." 
+      redirect_to root_path
+    end
   end
 end
