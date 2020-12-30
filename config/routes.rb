e996e604cf1efb3350dbe0e779f97f35e7c85605
Ggs91 <georges_atalla@hotmail.fr>
2020-12-30 21:58:28 +0100

    edit & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'events#index'
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   resources :events, only: [:new, :create, :show]
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
