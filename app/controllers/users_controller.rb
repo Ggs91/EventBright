59008628e82fe0bed386157c79b20ca7d2bd3807
Ggs91 <georges_atalla@hotmail.fr>
2020-12-30 16:01:45 +0100

    users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  before_action :restrict_acess_to_owner, only: [:show]
+
+  def show
+  end
+
+private
+  def restrict_acess_to_owner
+    unless User.find(params[:id]) == current_user 
+     flash[:warning] = "Sorry, but you are only allowed to view your own profile page." 
+      redirect_to root_path
+    end
+  end
+end
