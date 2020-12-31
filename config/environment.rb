e39d69bdef9db74437972c704af925390732d1fb
Ggs91 <georges_atalla@hotmail.fr>
2020-12-31 21:03:09 +0100

    add domain name to smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => ENV['HOST_DOMAIN_NAME'],
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
