112ea2424c6a3c2bbe647c81d20cd06680dcd85c
Ggs91 <georges_atalla@hotmail.fr>
2021-01-02 09:57:23 +0100

    undo skip template views

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module EventB
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.generators do |g|
-      g.template_engine nil #to skip views
       g.test_framework  nil #to skip test framework
       g.assets  false
       g.helper false
