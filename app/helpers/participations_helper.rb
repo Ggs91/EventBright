1d52845857f4e11b7716adba8a121356feeefb8e
Ggs91 <georges_atalla@hotmail.fr>
2021-01-02 17:29:15 +0100

    refactor method

--- app/helpers/participations_helper.rb
+++ app/helpers/participations_helper.rb
@@ -1,5 +1,5 @@
 module ParticipationsHelper
   def current_user_participation(event)
-    Participation.where(user:current_user, event:event)
+    Participation.where(user:current_user, event:event).first
   end
 end
