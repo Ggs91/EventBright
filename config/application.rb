2a90e4f4a0bee5bdbdb849a70dbb8d6d59d8084d
Ggs91 <georges_atalla@hotmail.fr>
2021-01-02 09:52:59 +0100

    skip template generating

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,14 @@ Bundler.require(*Rails.groups)
 module EventB
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
+    config.generators do |g|
+      g.template_engine nil #to skip views
+      g.test_framework  nil #to skip test framework
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+      g.skip_routes  true
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
