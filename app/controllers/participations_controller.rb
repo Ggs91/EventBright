c3821bcc5668c6720c10a57e356032b9481f4810
Ggs91 <georges_atalla@hotmail.fr>
2021-01-03 08:36:29 +0100

    amount set in before_action

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -2,6 +2,7 @@ class ParticipationsController < ApplicationController
   include EventsHelper
     before_action :authenticate_user!
     before_action :set_event, only: [:index, :new, :create]
+    before_action :amount_to_be_charged, only: [:new, :create]
     before_action :ensure_current_user_is_not_already_particitpant, only: [:new, :create]
     before_action :ensure_current_user_is_administrator, only: [:index]
     before_action :set_participation, only: [:destroy]
@@ -17,8 +18,8 @@ class ParticipationsController < ApplicationController
   end
 
   def create
-    # Amount in cents
-    @amount = @event.price
+    #Best practice for stripe integration
+    # https://rails.devcamp.com/trails/ruby-gem-walkthroughs/campsites/payment/guides/how-to-integrate-stripe-payments-in-a-rails-application-charges
 
     customer = StripeTool.create_customer(
                  email: params[:stripeEmail],
@@ -60,6 +61,12 @@ private
   def ensure_current_user_is_administrator
     current_user_is_administrator?(@event)
   end
+
+  def amount_to_be_charged
+    # Amount in cents
+    @amount = @event.price
+  end
+
 
   def ensure_current_user_is_not_already_particitpant #Dans la view le btn pour participer à un event change à "cancel" si current user est deja inscrit donc théroiquement pas possible de se ré-inscrir mais au cas ou 
     if current_user_already_participant?(@event)
