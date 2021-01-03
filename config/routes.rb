dc4a366041fe2a26bebd9a9018bd56c4ad687299
Ggs91 <georges_atalla@hotmail.fr>
2021-01-03 09:06:19 +0100

    add thanks route

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :participations, only: [:index, :new, :create, :destroy]
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  get 'thanks', to: 'participations#thanks', as: 'thanks'
 end
