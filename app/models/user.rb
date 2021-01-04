4eadc075d4c4f5ee2a070c254483e203a529c8ca
Ggs91 <georges_atalla@hotmail.fr>
2021-01-04 08:52:18 +0100

    authentication possible with either email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -23,5 +25,16 @@ class User < ApplicationRecord
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
+  end
+
+  # Extends devise authentication keys: email OR usernam are possible as authentication keys.
+  # Extends Devise to query via warden. This uses some SQL to query for either 
+  # the username or email fields given one or the other is supplied during form submission. 
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value",
+      { value: login.strip.downcase }]).first
   end
 end
