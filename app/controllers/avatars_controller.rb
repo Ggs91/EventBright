c14b5697a3cf901c1a0cc45dc3db0bdfa99e9d02
Ggs91 <georges_atalla@hotmail.fr>
2021-01-07 10:04:28 +0100

    create

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -1,2 +1,8 @@
 class AvatarsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    current_user.avatar.attach(params[:avatar])
+    redirect_back(fallback_location: root_path)
+  end
 end
