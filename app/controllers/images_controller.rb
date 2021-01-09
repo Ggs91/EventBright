63504457ea3e8863eb752e9cac393f0eb586336a
Ggs91 <georges_atalla@hotmail.fr>
2021-01-09 13:02:13 +0100

    destroy image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,16 +1,20 @@
 class ImagesController < ApplicationController
   before_action :set_event
-  
-#   def create
-#     @event.images.attach(params[:images]) if params[:images].present?
-#   end
+  before_action :set_image
 
-#   def destroy
-#   end
+
+  def destroy
+    @image.purge
+    redirect_to edit_event_path(@event)
+  end
 
 # private
 
-#   def set_event
-#     @event = Event.find(:event_id)
-#   end
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def set_image
+    @image = ActiveStorage::Attachment.find(params[:id])
+  end
 end
