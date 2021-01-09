9b26541e6655f8e2d78da7711aab47b0d04401ec
Ggs91 <georges_atalla@hotmail.fr>
2021-01-09 09:39:51 +0100

    generate images controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,16 @@
+class ImagesController < ApplicationController
+  before_action :set_event
+  
+#   def create
+#     @event.images.attach(params[:images]) if params[:images].present?
+#   end
+
+#   def destroy
+#   end
+
+# private
+
+#   def set_event
+#     @event = Event.find(:event_id)
+#   end
+end
