4c3c2d79c546134159e48ae98c426ca162ea822e
Ggs91 <georges_atalla@hotmail.fr>
2021-01-09 11:14:09 +0100

    add depedent:destroy to avatr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   attr_accessor :login
-  has_one_attached :avatar
+  has_one_attached :avatar, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
