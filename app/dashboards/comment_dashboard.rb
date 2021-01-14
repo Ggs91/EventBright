08620391e03862503da92df62d9606bb12f7559f
Ggs91 <georges_atalla@hotmail.fr>
2021-01-14 09:38:39 +0100

    remove belongs to event field

--- app/dashboards/comment_dashboard.rb
+++ app/dashboards/comment_dashboard.rb
@@ -8,7 +8,6 @@ class CommentDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    event: Field::BelongsTo,
     commenter: Field::BelongsTo.with_options(class_name: "User"),
     commentable: Field::Polymorphic,
     comments: Field::HasMany,
@@ -25,16 +24,15 @@ class CommentDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  event
   commenter
   commentable
   comments
+  id
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  event
   commenter
   commentable
   comments
@@ -49,7 +47,6 @@ class CommentDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  event
   commenter
   commentable
   comments
