835c0c28d15e415b9fd2d74b38bceb8a56907ebd
Ggs91 <georges_atalla@hotmail.fr>
2021-01-15 12:20:04 +0100

    admin authentication logic

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -9,7 +9,13 @@ module Admin
     before_action :authenticate_admin, :authenticate_user!
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      redirect_to '/', alert: 'Not authorized.' unless current_user && access_whitelist
+    end
+
+  private
+
+    def access_whitelist
+      current_user.try(:admin?)
     end
 
     # Override this value to specify the number of elements to display at a time
