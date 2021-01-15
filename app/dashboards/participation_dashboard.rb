14ea5500eac3d68818288ee61fff519ccae07641
Ggs91 <georges_atalla@hotmail.fr>
2021-01-15 12:22:15 +0100

    remove id field

--- app/dashboards/participation_dashboard.rb
+++ app/dashboards/participation_dashboard.rb
@@ -22,9 +22,9 @@ class ParticipationDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+  id
   user
   event
-  id
   stripe_customer_id
   ].freeze
 
