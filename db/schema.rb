d147a9959449fa3f423f5ec73828048289dfffbf
Ggs91 <georges_atalla@hotmail.fr>
2021-01-15 22:34:42 +0100

    add validated column to events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_104404) do
+ActiveRecord::Schema.define(version: 2021_01_15_213049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_01_15_104404) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "validated", default: false
     t.index ["administrator_id"], name: "index_events_on_administrator_id"
   end
 
