9af98106f27af62edcd979c926c39cdf4440321a
Ggs91 <georges_atalla@hotmail.fr>
2021-01-15 21:18:17 +0100

    customization comments dashboard

--- app/dashboards/comment_dashboard.rb
+++ app/dashboards/comment_dashboard.rb
@@ -24,21 +24,20 @@ class CommentDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+  id
   commenter
   commentable
   comments
-  id
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+  id
   commenter
   commentable
   comments
-  id
   content
-  commenter_id
   created_at
   updated_at
   ].freeze
@@ -47,11 +46,7 @@ class CommentDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  commenter
-  commentable
-  comments
   content
-  commenter_id
   ].freeze
 
   # COLLECTION_FILTERS
