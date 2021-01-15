519ed14a914c030044cc1d9ab466ceb5134fb2b9
Ggs91 <georges_atalla@hotmail.fr>
2021-01-15 12:21:50 +0100

    remove active storage fields

--- app/dashboards/event_dashboard.rb
+++ app/dashboards/event_dashboard.rb
@@ -8,8 +8,6 @@ class EventDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    images_attachments: Field::HasMany.with_options(class_name: "ActiveStorage::Attachment"),
-    images_blobs: Field::HasMany.with_options(class_name: "ActiveStorage::Blob"),
     administrator: Field::BelongsTo.with_options(class_name: "User"),
     participations: Field::HasMany,
     participants: Field::HasMany.with_options(class_name: "User"),
@@ -32,8 +30,6 @@ class EventDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-  images_attachments
-  images_blobs
   administrator
   participations
   ].freeze
@@ -41,8 +37,6 @@ class EventDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
-  images_attachments
-  images_blobs
   administrator
   participations
   participants
@@ -63,8 +57,6 @@ class EventDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-  images_attachments
-  images_blobs
   administrator
   participations
   participants
