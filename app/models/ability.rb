77111ae6e9237a908d400ee3d74f90394efea32d
Ggs91 <georges_atalla@hotmail.fr>
2021-01-19 11:48:08 +0100

    abilities for Events

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    # user ||= User.new # guest user (not logged in)
+    alias_action :create, :read, :update, :destroy, :to => :crud
+    
+    # Permissions for all users
+    can :read, Event, validated: true
+
+    # Additional permissions for login users
+    return unless user.present? 
+    can [:create, :update, :destroy], Event, administrator_id: user.id # scoped to the event's owner. For :new the gem will load @event = Event.new(administrator: current_user) 
+    can :submission_success, Event # Devise authorization will fire before it does
+
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
     #   if user.admin?
     #     can :manage, :all
     #   else
