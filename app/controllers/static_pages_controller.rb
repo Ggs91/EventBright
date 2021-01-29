9bddd56a0b73da568a6fd756ba81f6f21693c584
Ggs91 <georges_atalla@hotmail.fr>
2021-01-29 13:00:10 +0100

    add authorization for hompage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+  load_and_authorize_resource :event, :parent => false, only: [:home]
+
+  def home
+    @events = @events.latest(8).with_attached_images
+  end
+
   def contact
   end
 
