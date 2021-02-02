9a4a11ed940bc5883c474757991430429050aae7
Ggs91 <georges_atalla@hotmail.fr>
2021-02-02 20:57:32 +0100

    fix images seed in deployement

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,9 +50,16 @@ puts "#{User.all.count} users (20 user + 1 admin) created"
     validated: true,
   )
   e.participants.concat(User.all.sample(4))
-  e.images.attach(io: File.open(Rails.root.join('app/assets/images/sport.jpg')), filename: 'sport.jpg', content_type: 'image/jpg')
-  e.images.attach(io: File.open(Rails.root.join('app/assets/images/cooking.jpeg')), filename: 'cooking.jpeg', content_type: 'image/jpeg')
-  e.images.attach(io: File.open(Rails.root.join('app/assets/images/conference.jpg')), filename: 'conference.jpg', content_type: 'image/jpg')
+  image_path_1 = Rails.root.join('app', 'assets', 'images', 'sport.jpg')
+  image_path_2 = Rails.root.join('app', 'assets', 'images', 'cooking.jpeg')
+  image_path_3 = Rails.root.join('app', 'assets', 'images', 'conference.jpg')
+  e.images.attach(io: File.open(image_path_1), filename: 'sport.jpg', content_type: 'image/jpg')
+  e.images.attach(io: File.open(image_path_2), filename: 'cooking.jpeg', content_type: 'image/jpeg')
+  e.images.attach(io: File.open(image_path_3), filename: 'conference.jpg', content_type: 'image/jpg')
+
+  # e.images.attach(io: File.open(Rails.root.join('app/assets/images/sport.jpg')), filename: 'sport.jpg', content_type: 'image/jpg')
+  # e.images.attach(io: File.open(Rails.root.join('app/assets/images/cooking.jpeg')), filename: 'cooking.jpeg', content_type: 'image/jpeg')
+  # e.images.attach(io: File.open(Rails.root.join('app/assets/images/conference.jpg')), filename: 'conference.jpg', content_type: 'image/jpg')
 end
 puts "#{Event.all.count} events created"
 
