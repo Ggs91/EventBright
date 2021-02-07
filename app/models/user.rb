6be7557c4f3ffa7043e5a202789f9b151821cb7e
Ggs91 <georges_atalla@hotmail.fr>
2021-02-07 11:16:48 +0100

    put avatar association in the right section

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   attr_accessor :login
-  has_one_attached :avatar, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable, :trackable #, :omniauthable
+  :recoverable, :rememberable, :validatable,
+  :confirmable, :lockable, :timeoutable, :trackable #, :omniauthable
   # Associations 
+  has_one_attached :avatar, dependent: :destroy
   has_many :participations, dependent: :destroy
   has_many :attended_events, through: :participations, source: :event, dependent: :destroy
   has_many :administrated_events, foreign_key: "administrator_id", class_name: "Event", dependent: :destroy
